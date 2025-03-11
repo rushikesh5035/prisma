5fb9cba044426d49dfa042c966bf0162df52338c
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2025-03-11 23:42:51 +0530

    init

--- Prisma-postgres/src/index.ts
+++ Prisma-postgres/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello");
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+const createUser = async () => {
+  const response = await client.user.findFirst({
+    where: {
+      id: 2,
+    },
+  });
+  console.log(response);
+};
+
+createUser();
