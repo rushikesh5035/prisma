e861aaef7182d3984930d3698e1060d0904cfdba
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2025-03-12 00:07:12 +0530

    init

--- Prisma-postgres/dist/index.js
+++ Prisma-postgres/dist/index.js
@@ -8,15 +8,46 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const client_1 = require("@prisma/client");
+const app = (0, express_1.default)();
 const client = new client_1.PrismaClient();
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield client.user.findMany();
+    res.json({
+        users,
+    });
+}));
+app.get("/todos/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const users = yield client.user.findFirst({
+        where: {
+            id: Number(id),
+        },
+        select: {
+            todos: true,
+            username: true,
+            password: true,
+        },
+    });
+    res.json({
+        users,
+    });
+}));
+app.listen(3000);
 const createUser = () => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield client.user.findFirst({
+    const user = yield client.user.findFirst({
         where: {
             id: 2,
         },
+        include: {
+            todos: true,
+        },
     });
-    console.log(response);
+    console.log(user);
 });
-createUser();
+// createUser();
--- Prisma-postgres/src/index.ts
+++ Prisma-postgres/src/index.ts
@@ -1,14 +1,45 @@
+import express from "express";
 import { PrismaClient } from "@prisma/client";
 
+const app = express();
 const client = new PrismaClient();
 
+app.get("/users", async (req, res) => {
+  const users = await client.user.findMany();
+  res.json({
+    users,
+  });
+});
+
+app.get("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  const users = await client.user.findFirst({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      todos: true,
+      username: true,
+      password: true,
+    },
+  });
+  res.json({
+    users,
+  });
+});
+
+app.listen(3000);
+
 const createUser = async () => {
-  const response = await client.user.findFirst({
+  const user = await client.user.findFirst({
     where: {
       id: 2,
     },
+    include: {
+      todos: true,
+    },
   });
-  console.log(response);
+  console.log(user);
 };
 
-createUser();
+// createUser();
